5990ce875c46c0a95d9fdb84770d0c67120451fb
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-18 17:56:26 +0430

    feat(attrtypes): add ClusterMode enum class

---
+++ include/gvizard/attrtypes/clustermode.hpp
@@ -0,0 +1,14 @@
+#ifndef GVIZARD_ATTRTYPES_CLUSTERMODE_HPP_
+#define GVIZARD_ATTRTYPES_CLUSTERMODE_HPP_
+
+namespace gvizard::attrtypes {
+
+enum class ClusterMode {
+  none = 0,
+  local,
+  global,
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_CLUSTERMODE_HPP_
