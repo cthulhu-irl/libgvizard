598fcc4569eb4557008963f6b259c2a619e7e2cf
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-20 04:02:46 +0430

    feat(attrtypes/outputmode): add OutputMode enum

---
+++ include/gvizard/attrtypes/outputmode.hpp
@@ -0,0 +1,14 @@
+#ifndef GVIZARD_ATTRTYPES_OUTPUTMODE_HPP_
+#define GVIZARD_ATTRTYPES_OUTPUTMODE_HPP_
+
+namespace gvizard::attrtypes {
+
+enum class OutputMode {
+  breadthfirst = 0,
+  nodesfirst,
+  edgesfirst
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_OUTPUTMODE_HPP_
