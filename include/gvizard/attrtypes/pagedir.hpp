2097b17c5bdf0933bd681989f2dbfcc6031868b8
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-20 04:15:35 +0430

    feat(attrtypes/pagedir): add PageDir enum

---
+++ include/gvizard/attrtypes/pagedir.hpp
@@ -0,0 +1,19 @@
+#ifndef GVIZARD_ATTRTYPES_PAGEDIR_HPP_
+#define GVIZARD_ATTRTYPES_PAGEDIR_HPP_
+
+namespace gvizard::attrtypes {
+
+enum class PageDir {
+  BL = 0,
+  BR,
+  TL,
+  TR,
+  RB,
+  RT,
+  LB,
+  LT
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_PAGEDIR_HPP_
