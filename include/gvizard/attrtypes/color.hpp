b7943cf703685b3dd953839c811a1469835b63d2
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-19 16:29:05 +0430

    fix(attrtypes/color): constexpr and inline anything that desires so

--- include/gvizard/attrtypes/color.hpp
+++ include/gvizard/attrtypes/color.hpp
@@ -22,23 +22,23 @@ struct RGB {
   octet_t g = 0;
   octet_t b = 0;
 
-  static std::optional<RGB>
+  constexpr static std::optional<RGB>
   make(octet_t red, octet_t green, octet_t blue) noexcept
   {
     return RGB{ red, green, blue };
   }
 
-  octet_t min() const noexcept
+  constexpr octet_t min() const noexcept
   {
     return (r < g) ? ((r < b) ? r : b) : ((g < b) ? g : b);
   }
 
-  octet_t max() const noexcept
+  constexpr octet_t max() const noexcept
   {
     return (r > g) ? ((r > b) ? r : b) : ((g > b) ? g : b);
   }
 
-  uint32_t to_binary() const noexcept
+  constexpr uint32_t to_binary() const noexcept
   {
     return (r << 16) |
            (g <<  8) |
@@ -54,29 +54,29 @@ struct RGBA {
   octet_t b = 0;
   octet_t a = 255;
 
-  static std::optional<RGBA>
+  constexpr static std::optional<RGBA>
   make(octet_t red, octet_t green, octet_t blue) noexcept
   {
     return RGBA{ red, green, blue };
   }
 
-  static std::optional<RGBA>
+  constexpr static std::optional<RGBA>
   make(octet_t red, octet_t green, octet_t blue, octet_t alpha) noexcept
   {
     return RGBA{ red, green, blue, alpha };
   }
 
-  octet_t min() const noexcept
+  constexpr octet_t min() const noexcept
   {
     return (r < g) ? ((r < b) ? r : b) : ((g < b) ? g : b);
   }
 
-  octet_t max() const noexcept
+  constexpr octet_t max() const noexcept
   {
     return (r > g) ? ((r > b) ? r : b) : ((g > b) ? g : b);
   }
 
-  uint32_t to_binary() const noexcept
+  constexpr uint32_t to_binary() const noexcept
   {
     return (r << 24) |
            (g << 16) |
@@ -88,24 +88,25 @@ struct RGBA {
 struct HSV {
   using octet_t = double;
 
-  HSV(octet_t hue, octet_t saturation, octet_t value)
+  constexpr HSV(octet_t hue, octet_t saturation, octet_t value)
     : h(hue), s(saturation), v(value)
   {
-    if (h < 0.0 || 1.0 < h)
+    if (h < 0. || 1. < h)
       detail::invalid_argument("hue must be within (0, 1) range");
 
-    if (s < 0.0 || 1.0 < s)
+    if (s < 0. || 1. < s)
       detail::invalid_argument("saturation must be within (0, 1) range");
 
-    if (v < 0.0 || 1.0 < v)
+    if (v < 0. || 1. < v)
       detail::invalid_argument("value must be within (0, 1) range");
   }
 
-  static std::optional<HSV> make(octet_t h, octet_t s, octet_t v) noexcept
+  constexpr static std::optional<HSV>
+  make(octet_t h, octet_t s, octet_t v) noexcept
   {
-    if (h < 0.0 || 1.0 < h) return std::nullopt;
-    if (s < 0.0 || 1.0 < s) return std::nullopt;
-    if (v < 0.0 || 1.0 < v) return std::nullopt;
+    if (h < 0. || 1. < h) return std::nullopt;
+    if (s < 0. || 1. < s) return std::nullopt;
+    if (v < 0. || 1. < v) return std::nullopt;
 
     HSV hsv{};
 
@@ -116,9 +117,9 @@ struct HSV {
     return hsv;
   }
 
-  octet_t hue() const noexcept { return h; }
-  octet_t saturation() const noexcept { return s; }
-  octet_t value() const noexcept { return v; }
+  constexpr octet_t hue() const noexcept { return h; }
+  constexpr octet_t saturation() const noexcept { return s; }
+  constexpr octet_t value() const noexcept { return v; }
 
  private:
   HSV() noexcept {}
@@ -130,18 +131,18 @@ struct HSV {
 
 namespace color_convert {
   namespace detail {
-    static const HSV hsv_zero_default{0.0, 0.0, 0.0};
+    constexpr inline const HSV hsv_zero_default{0., 0., 0.};
   }
 
   template <typename From, typename To>
-  inline To convert(const From&) noexcept
+  constexpr inline To convert(const From&) noexcept
   {
     static_assert(sizeof(From) == 0,
         "given color types for color conversion are not supported.");
   }
 
   template <>
-  inline HSV
+  constexpr inline HSV
   convert<RGB, HSV>(const RGB& rgb) noexcept
   {
     uint8_t min = rgb.min(), max = rgb.max();
@@ -152,45 +153,43 @@ namespace color_convert {
     double v = double(max) / 255;
 
     double s = long(max - min) / max;
-    if (s == 0.0)
+    if (s == 0.)
       return detail::hsv_zero_default;
 
     // NOTE this will underflow if b > g, but won't be negative,
     //      not sure if abs would be better.
     uint8_t gb_diff = rgb.g - rgb.b;
 
-    double h;
+    double h = 0;
     if (max == rgb.r)
-      h = double(  0 + 43 * gb_diff / (max - min)) / 255;
+      h = double(  0 + 43 * gb_diff / (max - min)) / 255.;
 
     else if (max == rgb.g)
-      h = double( 83 + 43 * gb_diff / (max - min)) / 255;
+      h = double( 83 + 43 * gb_diff / (max - min)) / 255.;
 
     else // max == rgb.b
-      h = double(171 + 43 * gb_diff / (max - min)) / 255;
+      h = double(171 + 43 * gb_diff / (max - min)) / 255.;
 
     return HSV::make(h, s, v).value();
   }
 
   template <>
-  inline RGB
+  constexpr inline RGB
   convert<HSV, RGB>(const HSV& hsv) noexcept
   {
-    if (hsv.saturation() == 0.0)
+    if (hsv.saturation() == 0.)
       return RGB{0, 0, 0};
 
     uint8_t h = hsv.hue() * 255;
     uint8_t s = hsv.saturation() * 255;
     uint8_t v = hsv.value() * 255;
 
-    uint8_t region, remainder, p, q, t;
+    uint8_t region = h / 43;
+    uint8_t remainder = (h - (region * 43)) * 6;
 
-    region = h / 43;
-    remainder = (h - (region * 43)) * 6;
-
-    p = (v * (255 - s)) >> 8;
-    q = (v * (255 - ((s * remainder) >> 8))) >> 8;
-    t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
+    uint8_t p = (v * (255 - s)) >> 8;
+    uint8_t q = (v * (255 - ((s * remainder) >> 8))) >> 8;
+    uint8_t t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
 
     switch (region)
     {
@@ -204,28 +203,28 @@ namespace color_convert {
   }
 
   template <>
-  inline RGBA
+  constexpr inline RGBA
   convert<RGB, RGBA>(const RGB& color) noexcept
   {
     return { color.r, color.g, color.b };
   }
 
   template <>
-  inline RGB
+  constexpr inline RGB
   convert<RGBA, RGB>(const RGBA& color) noexcept
   {
     return { color.r, color.g, color.b };
   }
 
   template <>
-  inline HSV
+  constexpr inline HSV
   convert<RGBA, HSV>(const RGBA& color) noexcept
   {
     return convert<RGB, HSV>( convert<RGBA, RGB>(color) );
   }
 
   template <>
-  inline RGBA
+  constexpr inline RGBA
   convert<HSV, RGBA>(const HSV& color) noexcept
   {
     return convert<RGB, RGBA>( convert<HSV, RGB>(color) );
@@ -233,26 +232,26 @@ namespace color_convert {
 
   // std::is_same_v<From, To> == true
   template <>
-  inline RGB
+  constexpr inline RGB
   convert<RGB, RGB>(const RGB& color) noexcept { return color; }
 
   template <>
-  inline RGBA
+  constexpr inline RGBA
   convert<RGBA, RGBA>(const RGBA& color) noexcept { return color; }
 
   template <>
-  inline HSV
+  constexpr inline HSV
   convert<HSV, HSV>(const HSV& color) noexcept { return color; }
 
   template <typename To>
   struct visitor final {
-    To operator()(const HSV& color) const
+    constexpr To operator()(const HSV& color) const
     { return convert<HSV, To>(color); }
 
-    To operator()(const RGB& color) const
+    constexpr To operator()(const RGB& color) const
     { return convert<RGB, To>(color); }
 
-    To operator()(const RGBA& color) const
+    constexpr To operator()(const RGBA& color) const
     { return convert<RGBA, To>(color); }
   };
 }  // namespace color_convert
@@ -262,20 +261,20 @@ struct Color {
 
   color_variant_t color;
 
-  Color(const color_variant_t& clr) : color(clr) {}
-  Color(color_variant_t&& clr) : color(std::move(clr)) {}
+  constexpr Color(const color_variant_t& clr) : color(clr) {}
+  constexpr Color(color_variant_t&& clr) : color(std::move(clr)) {}
 
-  Color(RGB clr) : color(std::move(clr)) {}
-  Color(RGBA clr) : color(std::move(clr)) {}
-  Color(HSV clr) : color(std::move(clr)) {}
+  constexpr Color(RGB clr) : color(std::move(clr)) {}
+  constexpr Color(RGBA clr) : color(std::move(clr)) {}
+  constexpr Color(HSV clr) : color(std::move(clr)) {}
 
-  static std::optional<Color>
+  constexpr static std::optional<Color>
   make_rgb(RGB::octet_t r, RGB::octet_t g, RGB::octet_t b) noexcept
   {
     return Color{ RGB{ r, g, b } };
   }
 
-  static std::optional<Color>
+  constexpr static std::optional<Color>
   make_rgba(
     RGBA::octet_t r,
     RGBA::octet_t g,
@@ -285,7 +284,7 @@ struct Color {
     return Color{ RGBA{ r, g, b, a } };
   }
 
-  static std::optional<Color>
+  constexpr static std::optional<Color>
   make_hsv(HSV::octet_t h, HSV::octet_t s, HSV::octet_t v) noexcept
   {
     auto hsv = HSV::make(h, s, v);
@@ -296,7 +295,7 @@ struct Color {
   }
 
   template <typename T>
-  T as() const noexcept
+  constexpr T as() const noexcept
   {
     return std::visit(color_convert::visitor<T>{}, color);
   }
