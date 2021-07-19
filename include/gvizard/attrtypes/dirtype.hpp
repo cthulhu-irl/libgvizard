ac1cee0d8aeea083feb8ad248bb16f7099bc3a81
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-20 02:58:19 +0430

    feat(attrtypes/dirtype): add DirType enum

---
+++ include/gvizard/attrtypes/dirtype.hpp
@@ -0,0 +1,15 @@
+#ifndef GVIZARD_ATTRTYPES_DIRTYPE_HPP_
+#define GVIZARD_ATTRTYPES_DIRTYPE_HPP_
+
+namespace gvizard::attrtypes {
+
+enum class DirType {
+  none = 0,
+  forward,
+  back,
+  both
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_DIRTYPE_HPP_
