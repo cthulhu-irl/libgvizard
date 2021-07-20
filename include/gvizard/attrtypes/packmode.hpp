91ddfd596de99c806ccac7d7a9fbadaa37ce3cb8
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-20 10:02:12 +0430

    feat(attrtypes/packmode): add PackModeEnum and PackModeArrayFlag enums and PackMode struct

---
+++ include/gvizard/attrtypes/packmode.hpp
@@ -0,0 +1,48 @@
+#ifndef GVIZARD_ATTRTYPES_PACKMODE_HPP_
+#define GVIZARD_ATTRTYPES_PACKMODE_HPP_
+
+#include <cstdint>
+
+namespace gvizard::attrtypes {
+
+enum class PackModeEnum : uint8_t {
+  node = 0,
+  clust,
+  graph,
+  array
+};
+
+enum class PackModeArrayFlag : uint8_t {
+  none = 0,
+  column,
+  user,
+  top,
+  bottom,
+  left,
+  right
+};
+
+struct PackMode final {
+  PackModeEnum mode = PackModeEnum::node;
+  PackModeArrayFlag flag = PackModeArrayFlag::none;
+  std::size_t number = 0;
+
+  constexpr PackMode& normalize() noexcept
+  {
+    if (mode != PackModeEnum::array) {
+      flag = PackModeArrayFlag::none;
+      number = 0;
+    }
+
+    return *this;
+  }
+
+  constexpr PackMode normalized() const noexcept
+  {
+    return (mode == PackModeEnum::array) ? *this : PackMode{ mode };
+  }
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_PACKMODE_HPP_
