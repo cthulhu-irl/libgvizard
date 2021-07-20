67cfe9110484dd1bd2490154120e133e94fa5c65
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-20 19:46:28 +0430

    feat(attrtypes/portpos): add CompassPoint enum and PortPos struct

---
+++ include/gvizard/attrtypes/portpos.hpp
@@ -0,0 +1,30 @@
+#ifndef GVIZARD_ATTRTYPES_PORTPOS_HPP_
+#define GVIZARD_ATTRTYPES_PORTPOS_HPP_
+
+#include <cstdint>
+#include <string>
+#include <optional>
+
+namespace gvizard::attrtypes {
+
+enum class CompassPoint : uint8_t {
+  _default = 0,
+  center,
+  north,
+  south,
+  west,
+  east,
+  north_east,
+  north_west,
+  south_east,
+  south_west,
+};
+
+struct PortPos final {
+  std::optional<std::string> port = std::nullopt;
+  CompassPoint compass = CompassPoint::_default;
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_PORTPOS_HPP_
