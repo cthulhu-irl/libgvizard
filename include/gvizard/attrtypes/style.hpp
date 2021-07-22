f81179a830a90a871bf3126a5dea4de0b618d735
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-23 01:17:35 +0430

    feat(attrtypes/style): add CommonStyle, NodeStyleOnly, EdgeStyleOnly, ClusterStyleOnly enums and StyleItemm BuiltinStyleItem and Style structs

---
+++ include/gvizard/attrtypes/style.hpp
@@ -0,0 +1,117 @@
+#ifndef GVIZARD_ATTRTYPES_STYLE_HPP_
+#define GVIZARD_ATTRTYPES_STYLE_HPP_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <variant>
+#include <algorithm>
+
+#include "gvizard/utils.hpp"
+
+namespace gvizard::attrtypes {
+
+enum class CommonStyle : uint8_t {
+  none = 0,
+  dashed,
+  dotted,
+  solid,
+  invis,
+  bold
+};
+
+enum class NodeStyleOnly : uint8_t {
+  none = 0,
+  filled,
+  striped,
+  wedged,
+  diagonals,
+  rounded,
+  radial
+};
+
+enum class EdgeStyleOnly : uint8_t {
+  none = 0,
+  tapered
+};
+
+enum class ClusterStyleOnly : uint8_t {
+  none = 0,
+  filled,
+  striped,
+  rounded,
+  radial
+};
+
+struct StyleItem final {
+  std::string name;
+  std::vector<std::string> args;
+};
+
+struct BuiltinStyleItem final {
+  using builtin_style_type = std::variant<CommonStyle, NodeStyleOnly,
+                                        EdgeStyleOnly, ClusterStyleOnly>;
+  builtin_style_type name;
+  std::vector<std::string> args;
+
+  StyleItem to_style_item() const
+  {
+    return StyleItem{
+      std::visit(
+        [](auto style) -> std::string
+        {
+          using enum_type = decltype(style);
+          return bool(style)
+            ? std::string(utils::EnumHelper<enum_type>::to_str(style))
+            : std::string("");
+        },
+        name
+      ),
+      args
+    };
+  }
+};
+
+struct Style final {
+  using item_type = std::variant<BuiltinStyleItem, StyleItem>;
+
+  constexpr static auto to_style_item =
+    [](const item_type& item)
+    {
+      return std::visit(
+        utils::LambdaVisitor{
+          [](const StyleItem& arg) { return arg; },
+          [](const BuiltinStyleItem& arg)
+            { return arg.to_style_item(); }
+        },
+        item
+      );
+    };
+
+  std::vector<item_type> items;
+
+  Style& all_style_items()
+  {
+    std::transform(
+        std::begin(items), std::end(items),
+        std::begin(items), to_style_item
+    );
+    return *this;
+  }
+
+  std::vector<StyleItem> to_style_item_vector() const
+  {
+    std::vector<StyleItem> style_items{};
+    style_items.reserve(items.size());
+
+    std::transform(
+        std::begin(items), std::end(items),
+        std::begin(style_items), to_style_item
+    );
+    return style_items;
+  }
+};
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_STYLE_HPP_
