518a874393078e1c986784835a31ffad912f2385
cthulhu-irl <xh4x0r3r@gmail.com>
2021-07-25 22:40:25 +0430

    feat(contracts): add Ranged, LessEqed, and GreaterEqed contracts

---
+++ include/gvizard/contracts.hpp
@@ -0,0 +1,33 @@
+#ifndef GVIZARD_CONTRACTS_HPP_
+#define GVIZARD_CONTRACTS_HPP_
+
+#include "gvizard/utils.hpp"
+
+namespace gvizard::contracts {
+
+template <typename T, T min, T max>
+constexpr auto ranged_constraint =
+  +[](const T& val) { return (min <= val) && (val <= max); };
+
+template <typename T, T min, T max>
+using Ranged = utils::Contract<T, ranged_constraint<T, min, max>>;
+
+
+template <typename T, T base>
+constexpr auto lesseqed_constraint =
+  +[](const T& val) { return base >= val; };
+
+template <typename T, T base>
+using LessEqed = utils::Contract<T, lesseqed_constraint<T, base>>;
+
+
+template <typename T, T base>
+constexpr auto greatereqed_constraint =
+  +[](const T& val) { return base <= val; };
+
+template <typename T, T base>
+using GreaterEqed = utils::Contract<T, greatereqed_constraint<T, base>>;
+
+}  // namespace gvizard::contracts
+
+#endif  // GVIZARD_CONTRACTS_HPP_
