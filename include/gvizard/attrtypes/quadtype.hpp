0a5d68b15d55541c6f37686a288d8817837475d8
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-01 02:20:06 +0430

    feat(attrtypes/quadtype): add utils::Converter specialization for QuadType/bool conversion and vice versa

--- include/gvizard/attrtypes/quadtype.hpp
+++ include/gvizard/attrtypes/quadtype.hpp
@@ -3,7 +3,10 @@
 
 #include <cstdint>
 
-namespace gvizard::attrtypes {
+#include "gvizard/utils.hpp"
+
+namespace gvizard {
+namespace attrtypes {
 
 enum class QuadType : uint8_t {
   none = 0,
@@ -16,6 +19,28 @@ constexpr inline QuadType quadtype_from_bool(bool type)
   return type ? QuadType::normal : QuadType::none;
 }
 
-}  // namespace gvizard::attrtypes
+}  // namespace attrtypes
+
+namespace utils {
+
+template <>
+struct Converter<attrtypes::QuadType, bool> final {
+  constexpr static attrtypes::QuadType convert(bool qtype)
+  {
+    return attrtypes::quadtype_from_bool(qtype);
+  }
+};
+
+template <>
+struct Converter<bool, attrtypes::QuadType> final {
+  constexpr static bool convert(attrtypes::QuadType qtype)
+  {
+    return qtype == attrtypes::QuadType::normal;
+  }
+};
+
+}  // namespace utils
+
+}  // namespace gvizard
 
 #endif  // GVIZARD_ATTRTYPES_QUADTYPE_HPP_
