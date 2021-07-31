242cebedb1b462279a0abada04a471bfc1489c68
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-01 02:13:17 +0430

    feat(attrtypes/color): add WeightedColor class

--- include/gvizard/attrtypes/color.hpp
+++ include/gvizard/attrtypes/color.hpp
@@ -165,6 +165,44 @@ struct NamedColor final {
   }
 };
 
+template <typename ColorT>
+class WeightedColor final {
+ public:
+  using color_type = ColorT;
+
+ private:
+  color_type color_;
+  double     weight_;
+
+ public:
+  constexpr WeightedColor(ColorT color, double weight)
+    : color_(color), weight_(weight)
+  {
+    if (0. > weight_ || weight_ > 1.)
+      throw std::invalid_argument("given weight is out of range. (0, 1)");
+  }
+
+  constexpr double get_color() const noexcept(noexcept(color_type(color_)))
+  {
+    return color_;
+  }
+
+  constexpr double get_weight() const noexcept { return weight_; }
+
+  constexpr WeightedColor& set_color(const color_type& color)
+  {
+    color_ = color;
+  }
+
+  constexpr WeightedColor& set_weight(double weight)
+  {
+    if (0. > weight || weight > 1.)
+      throw std::invalid_argument("given weight is out of range. (0, 1)");
+
+    weight_ = weight;
+  }
+};
+
 namespace color_convert {
   // NOTE NamedColor doesn't need convert specialization
   //      as it is implicitly convertible to its inner color_type.
