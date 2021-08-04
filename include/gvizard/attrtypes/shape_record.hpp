b00c3161560dc593a0cb72f3d1ab8c5e1b572db9
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-04 05:19:49 +0430

    feat(attrtypes/shape_record): use template contained specifier pattern for structures

--- include/gvizard/attrtypes/shape_record.hpp
+++ include/gvizard/attrtypes/shape_record.hpp
@@ -5,16 +5,24 @@
 #include <vector>
 #include <variant>
 
+template <typename NameT = std::string, typename PortT = std::string>
 struct RecordFieldId final {
-  std::string name{};
-  std::string port{};
+  using name_type = NameT;
+  using port_type = PortT;
+
+  name_type name{};
+  port_type port{};
 };
 
+template <typename FieldIdT = RecordFieldId<std::string, std::string>,
+          template <typename, typename...> typename Vec = std::vector,
+          typename ...VecArgs>
 struct RecordShape final {
-  using fieldid_list_type = std::vector<RecordFieldId>;
-  using field_type = std::variant<RecordFieldId, fieldid_list_type>;
+  using fieldid_type = FieldIdT;
+  using fieldid_list_type = Vec<fieldid_type, VecArgs...>;
+  using field_type = std::variant<fieldid_type, fieldid_list_type>;
 
-  std::vector<field_type> fields{};
+  Vec<field_type, VecArgs...> fields{};
 
   template <typename ...Args>
   RecordShape(const field_type& first, Args&& ...args)
