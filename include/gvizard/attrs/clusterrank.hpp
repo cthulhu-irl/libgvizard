27f83a6826fd4616d73a84f0e9d94ed6fc6cd41d
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-10 23:02:10 +0430

    feat(attrs/clusterrank): add ClusterRank attribute struct

---
+++ include/gvizard/attrs/clusterrank.hpp
@@ -0,0 +1,34 @@
+#ifndef GVIZARD_ATTRS_CLUSTERRANK_HPP_
+#define GVIZARD_ATTRS_CLUSTERRANK_HPP_
+
+#include "gvizard/attribute.hpp"
+#include "gvizard/attrtypes/clustermode.hpp"
+
+namespace gvizard::attrs {
+
+struct ClusterRank
+  : public AttributeBase<ClusterRank, attrtypes::ClusterMode>
+{
+  using value_type = attrtypes::ClusterMode;
+
+  constexpr static const char * const name = "clusterrank";
+
+  constexpr ClusterRank() : AttributeBase(get_default_value()) {}
+  constexpr ClusterRank(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return attrtypes::ClusterMode::local;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_CLUSTERRANK_HPP_
