fc9d6ba19277ec2c241e63b2eec1c3c9b477fae6
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-10 20:44:01 +0430

    feat(attrs/bgcolor): add BGColor attribute struct

---
+++ include/gvizard/attrs/bgcolor.hpp
@@ -0,0 +1,43 @@
+#ifndef GVIZARD_ATTRS_BGCOLOR_HPP_
+#define GVIZARD_ATTRS_BGCOLOR_HPP_
+
+#include <optional>
+
+#include "gvizard/attribute.hpp"
+#include "gvizard/attrtypes/color.hpp"
+
+namespace gvizard::attrs {
+
+template <typename GradientType = attrtypes::ColorType>
+using BGColorValueType =
+  std::optional<std::variant<attrtypes::ColorType, GradientType>>;
+
+struct BGColor final
+  : public AttributeBase<BGColor, BGColorValueType<attrtypes::ColorType>>
+{
+  using value_type = BGColorValueType<attrtypes::ColorType>;
+
+  constexpr static const char * const name = "bb";
+
+  constexpr BGColor() : AttributeBase(get_default_value()) {}
+  constexpr BGColor(const value_type& value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return std::nullopt;
+  }
+
+  constexpr static bool is_default(const value_type& value) noexcept
+  {
+    return !value;
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_BGCOLOR_HPP_
