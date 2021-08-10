1146d28e7998f864c46e44f86b8a096d652e2c0a
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-11 01:38:38 +0430

    feat(attrs/colorscheme): add ColorScheme attribute struct

---
+++ include/gvizard/attrs/colorscheme.hpp
@@ -0,0 +1,46 @@
+#ifndef GVIZARD_ATTRS_COLORSCHEME_HPP_
+#define GVIZARD_ATTRS_COLORSCHEME_HPP_
+
+#include <string>
+
+#include "gvizard/utils.hpp"
+
+#include "gvizard/attribute.hpp"
+#include "gvizard/colors/general.hpp"
+
+namespace gvizard::attrs {
+
+struct ColorScheme : public AttributeBase<ColorScheme, colors::SchemeEnum> {
+  using value_type = colors::SchemeEnum;
+
+  constexpr static const char * const name = "colorscheme";
+
+  constexpr ColorScheme() : AttributeBase(get_default_value()) {}
+  constexpr ColorScheme(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return colors::SchemeEnum::X11;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return true;
+  }
+
+  operator std::string() const
+  {
+    return std::string(
+      utils::EnumHelper<colors::SchemeEnum>::to_str(value_)
+    );
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_COLORSCHEME_HPP_
