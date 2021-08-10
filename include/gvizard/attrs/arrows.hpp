550216603733f531bab31109d0a4aa958a7f7a0c
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-10 06:50:36 +0430

    feat(attrs/arrows): add ArrowHead, ArrowTail, and ArrowSize attribute static structs

--- include/gvizard/attrs/arrows.hpp
+++ include/gvizard/attrs/arrows.hpp
@@ -9,28 +9,84 @@
 namespace gvizard::attrs {
 
 struct ArrowHead final
-  : AttributeBase<ArrowHead, std::optional<attrtypes::ArrowType>>
+  : public AttributeBase<ArrowHead, attrtypes::ArrowType>
 {
-  using value_type = std::optional<attrtypes::ArrowType>;
+  using value_type = attrtypes::ArrowType;
 
   constexpr static const char * const name = "arrowhead";
 
-  ArrowHead() : AttributeBase(get_default_value()) {}
-  ArrowHead(value_type value) : AttributeBase(std::move(value)) {}
+  constexpr ArrowHead() : AttributeBase(get_default_value()) {}
+  constexpr ArrowHead(value_type arrow)
+    : AttributeBase(std::move(arrow))
+  {}
 
   constexpr static value_type get_default_value() noexcept
   {
-    return std::nullopt;
+    return attrtypes::ArrowType{ attrtypes::arrowshapes::normal };
   }
 
-  constexpr static bool is_default(const value_type& value)
+  constexpr static bool is_default(const value_type& arrow) noexcept
   {
-    return !bool(value);
+    return arrow == attrtypes::arrowshapes::none
+        || arrow == attrtypes::arrowshapes::normal;
   }
 
-  constexpr static bool constraint(const value_type& value) noexcept
+  constexpr static bool constraint(const value_type&) noexcept
   {
     return true;
+  }
+};
+
+struct ArrowTail final
+  : public AttributeBase<ArrowHead, attrtypes::ArrowType>
+{
+  using value_type = attrtypes::ArrowType;
+
+  constexpr static const char * const name = "arrowtail";
+
+  constexpr ArrowTail() : AttributeBase(get_default_value()) {}
+  constexpr ArrowTail(value_type arrow)
+    : AttributeBase(std::move(arrow))
+  {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return attrtypes::ArrowType{ attrtypes::arrowshapes::normal };
+  }
+
+  constexpr static bool is_default(const value_type& arrow) noexcept
+  {
+    return arrow == attrtypes::arrowshapes::none
+        || arrow == attrtypes::arrowshapes::normal;
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
+};
+
+struct ArrowSize final : public AttributeBase<ArrowSize, double> {
+  using value_type = double;
+
+  constexpr static const char * const name = "arrowsize";
+
+  constexpr ArrowSize() : AttributeBase(get_default_value()) {}
+  constexpr ArrowSize(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return 1.0;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value > 0.0;
   }
 };
 
