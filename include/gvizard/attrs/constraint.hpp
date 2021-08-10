e764027d4717655c20f7056fe98a6c5c27e9f8f5
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-11 02:09:20 +0430

    feat(attrs/constraint): add Constraint attribute struct

---
+++ include/gvizard/attrs/constraint.hpp
@@ -0,0 +1,28 @@
+#ifndef GVIZARD_ATTRS_CONSTRAINT_HPP_
+#define GVIZARD_ATTRS_CONSTRAINT_HPP_
+
+#include "gvizard/attribute.hpp"
+
+namespace gvizard::attrs {
+
+struct Constraint final : public AttributeBase<Constraint, bool> {
+  using value_type = bool;
+
+  constexpr static const char * const name = "constraint";
+
+  constexpr explicit Constraint() : AttributeBase(get_default_value()) {}
+  constexpr explicit Constraint(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return true; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_CONSTRAINT_HPP_
