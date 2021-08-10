ade4bb08ffd7bc8f639422d49030342c902ed11f
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-11 01:49:33 +0430

    feat(attrs/comment): add Comment attribute struct

---
+++ include/gvizard/attrs/comment.hpp
@@ -0,0 +1,33 @@
+#ifndef GVIZARD_ATTRS_COMMENT_HPP_
+#define GVIZARD_ATTRS_COMMENT_HPP_
+
+#include <string>
+
+#include "gvizard/attribute.hpp"
+
+namespace gvizard::attrs {
+
+struct Comment final : public AttributeBase<Comment, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "comment";
+
+  explicit Comment() : AttributeBase(get_default_value()) {}
+  explicit Comment(const value_type& value) : AttributeBase(value) {}
+  explicit Comment(value_type&& value)
+    : AttributeBase(std::move(value))
+  {}
+
+  static value_type get_default_value() { return ""; }
+
+  static bool is_default(const value_type& value)
+  {
+    return value.size() == 0;
+  }
+
+  constexpr static bool constraint(const value_type&) { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_COMMENT_HPP_
