70ef622aed2195472ed2d46e2f94f7db0ebee90f
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-10 22:22:34 +0430

    feat(attrs/clazz): add Clazz attribute struct (svg class attr)

---
+++ include/gvizard/attrs/clazz.hpp
@@ -0,0 +1,30 @@
+#ifndef GVIZARD_ATTRS_CLAZZ_HPP_
+#define GVIZARD_ATTRS_CLAZZ_HPP_
+
+#include <string>
+
+#include "gvizard/attribute.hpp"
+
+namespace gvizard::attrs {
+
+struct Clazz : public AttributeBase<Clazz, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "class";
+
+  explicit Clazz() : AttributeBase(get_default_value()) {}
+  explicit Clazz(const value_type& value) : AttributeBase(value) {}
+  explicit Clazz(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return ""; }
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type& value) { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_CLAZZ_HPP_
