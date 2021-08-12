b4539aeebc5f8bb7aa62f1b80a5aae6ab7a3b67a
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-12 11:05:07 +0430

    feat(attrtypes/viewport): add operator== and operator!= overloads to ViewPortXY and ViewPortS

--- include/gvizard/attrtypes/viewport.hpp
+++ include/gvizard/attrtypes/viewport.hpp
@@ -12,15 +12,43 @@ struct ViewPortXY final {
   Point2D<double> size = {0., 0.};
   double zoom = 1.;
   Point2D<double> center{};
+
+  constexpr bool operator==(const ViewPortXY& other) const noexcept
+  {
+    return size == other.size
+        && zoom == other.zoom
+        && center == other.center;
+  }
+
+  constexpr bool operator!=(const ViewPortXY& other) const noexcept
+  {
+    return size == other.size
+        && zoom == other.zoom
+        && center == other.center;
+  }
 };
 
 struct ViewPortS final {
   Point2D<double> size = {0., 0.};
   double zoom = 1.;
   std::string center{};
+
+  constexpr bool operator==(const ViewPortS& other) const noexcept
+  {
+    return size == other.size
+        && zoom == other.zoom
+        && center == other.center;
+  }
+
+  constexpr bool operator!=(const ViewPortS& other) const noexcept
+  {
+    return size == other.size
+        && zoom == other.zoom
+        && center == other.center;
+  }
 };
 
-using view_port_type = std::variant<ViewPortXY, ViewPortS>;
+using ViewPortType = std::variant<ViewPortXY, ViewPortS>;
 
 }  // namespace gvizard::attrtypes
 
