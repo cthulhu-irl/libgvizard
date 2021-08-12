6940f5605f0458249a0b88cc685e8f9e51bda464
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-13 01:37:50 +0430

    feat(attrs/damping): add Damping attribute struct

---
+++ include/gvizard/attrs/damping.hpp
@@ -0,0 +1,34 @@
+#ifndef GVIZARD_ATTRS_DAMPING_HPP_
+#define GVIZARD_ATTRS_DAMPING_HPP_
+
+#include "gvizard/attribute.hpp"
+
+namespace gvizard::attrs {
+
+struct Damping final : public AttributeBase<Damping, double> {
+  using value_type = double;
+
+  constexpr static const char * const name = "damping";
+
+  constexpr Damping() : AttributeBase(get_default_value()) {}
+  constexpr Damping(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return 0.99;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= 0.0;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_DAMPING_HPP_
