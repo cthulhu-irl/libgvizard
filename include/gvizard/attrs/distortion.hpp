9989a463f2d544bd217b90707fef34da75f4a20a
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-16 08:33:48 +0430

    feat(attrs/distortion): add Distortion attribute struct

---
+++ include/gvizard/attrs/distortion.hpp
@@ -0,0 +1,34 @@
+#ifndef GVIZARD_ATTRS_DISTORTION_HPP_
+#define GVIZARD_ATTRS_DISTORTION_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct Distortion final : public AttributeBase<Distortion, double> {
+  using value_type = double;
+
+  constexpr static const char * const name = "distortion";
+
+  constexpr explicit Distortion() : AttributeBase() {}
+  constexpr explicit Distortion(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return 0.0;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= -100.0;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_DISTORTION_HPP_
