a54067d82ef8c1d46effd9aac56b31b11b249417
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-16 08:24:25 +0430

    feat(attrs/dir): add Dir attribute struct

---
+++ include/gvizard/attrs/dir.hpp
@@ -0,0 +1,32 @@
+#ifndef GVIZARD_ATTRS_DIR_HPP_
+#define GVIZARD_ATTRS_DIR_HPP_
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/dirtype.hpp>
+
+namespace gvizard::attrs {
+
+struct Dir final : public AttributeBase<Dir, attrtypes::DirType> {
+  using value_type = attrtypes::DirType;
+
+  constexpr static const char * const name = "dir";
+
+  constexpr Dir() : AttributeBase() {}
+  constexpr Dir(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return attrtypes::DirType::none;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_DIR_HPP_
