1bb87af5d6eb59990c7ff78bb4614e57becea1f0
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-16 05:27:28 +0430

    feat(attrs/defaultdist): add DefaultDist attribute struct

---
+++ include/gvizard/attrs/defaultdist.hpp
@@ -0,0 +1,36 @@
+#ifndef GVIZARD_ATTRS_DEFAULTDIST_HPP_
+#define GVIZARD_ATTRS_DEFAULTDIST_HPP_
+
+#include <limits>
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct DefaultDist final : public AttributeBase<DefaultDist, double> {
+  using value_type = double;
+
+  constexpr static const char * const name = "defaultdist";
+
+  constexpr explicit DefaultDist() : AttributeBase() {}
+  constexpr explicit DefaultDist(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return 0.; // can't compute this here: 1 + (avg. len) * sqrt(|V|)
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= std::numeric_limits<double>::epsilon();
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_DEFAULTDIST_HPP_
