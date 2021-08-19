64b1d416311b92cb4e9cbdb7493ee04d5884569e
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-19 23:01:58 +0430

    feat(attrs/fillcolor): add FillColor attribute struct

---
+++ include/gvizard/attrs/fillcolor.hpp
@@ -0,0 +1,43 @@
+#ifndef GVIZARD_ATTRS_FILLCOLOR_HPP_
+#define GVIZARD_ATTRS_FILLCOLOR_HPP_
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/color.hpp>
+#include <gvizard/colors/x11.hpp>
+
+namespace gvizard::attrs {
+
+using FillColorType =
+  std::variant<
+    attrtypes::ColorType,
+    attrtypes::ColorList<attrtypes::ColorType>>;
+
+struct FillColor final : public AttributeBase<FillColor, FillColorType> {
+  using value_type = FillColorType;
+
+  constexpr static const char * const name = "fillcolor";
+
+  explicit FillColor() : AttributeBase() {}
+  explicit FillColor(const value_type& value) : AttributeBase(value) {}
+  explicit FillColor(value_type&& value)
+    : AttributeBase(std::move(value))
+  {}
+
+  static value_type get_default_value()
+  {
+    return attrtypes::ColorType(
+      colors::X11Color{colors::X11ColorEnum::lightgrey}
+    );
+  }
+
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_FILLCOLOR_HPP_
