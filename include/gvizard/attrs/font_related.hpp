b0a3cf789d8b71007534c7aa9eec924a65017e9d
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-20 01:38:59 +0430

    feat(attrs/font_related): add FontName attribute struct

--- include/gvizard/attrs/font_related.hpp
+++ include/gvizard/attrs/font_related.hpp
@@ -1,6 +1,8 @@
 #ifndef GVIZARD_ATTRS_FONT_RELATED_HPP_
 #define GVIZARD_ATTRS_FONT_RELATED_HPP_
 
+#include <string>
+
 #include <gvizard/attribute.hpp>
 #include <gvizard/attrtypes/color.hpp>
 #include <gvizard/colors/x11.hpp>
@@ -31,6 +33,28 @@ struct FontColor final
   {
     return true;
   }
+};
+
+
+struct FontName final : public AttributeBase<FontName, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "fontname";
+
+  explicit FontName() : AttributeBase() {}
+  explicit FontName(const value_type& value) : AttributeBase(value) {}
+  explicit FontName(value_type&& value)
+    : AttributeBase(std::move(value))
+  {}
+
+  static value_type get_default_value() { return "Times-Roman"; }
+
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type&) noexcept { return false; }
 };
 
 }  // namespace gvizard::attrs
