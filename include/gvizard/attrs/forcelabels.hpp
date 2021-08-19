cd3e34633ac2b21910504d836e98e24a1d3db8cc
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-20 03:55:13 +0430

    feat(attrs/forcelabels: add ForceLabels attribute struct

---
+++ include/gvizard/attrs/forcelabels.hpp
@@ -0,0 +1,30 @@
+#ifndef GVIZARD_ATTRS_FORCELABELS_HPP_
+#define GVIZARD_ATTRS_FORCELABELS_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct ForceLabels final : public AttributeBase<ForceLabels, bool> {
+  using value_type = bool;
+
+  constexpr static const char * const name = "forcelabels";
+
+  constexpr explicit ForceLabels() : AttributeBase(get_default_value()) {}
+  constexpr explicit ForceLabels(value_type value)
+    : AttributeBase(value)
+  {}
+
+  constexpr static value_type get_default_value() noexcept { return true; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_FORCELABELS_HPP_
