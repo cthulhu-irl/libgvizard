275d80989395db59d88ddc8c0cb9ad48e9d068ab
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-21 00:43:14 +0430

    feat(attrs/height): add Height attribute struct

---
+++ include/gvizard/attrs/height.hpp
@@ -0,0 +1,34 @@
+#ifndef GVIZARD_ATTRS_HEIGHT_HPP_
+#define GVIZARD_ATTRS_HEIGHT_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct Height final : public AttributeBase<Height, double> {
+  using value_type = double;
+
+  constexpr static const char * const name = "height";
+
+  constexpr explicit Height() : AttributeBase(get_default_value()) {}
+  constexpr explicit Height(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return 0.5;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= 0.02;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_HEIGHT_HPP_
