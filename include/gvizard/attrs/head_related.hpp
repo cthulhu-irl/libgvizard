f55073e709c2ffb358e843afa448cc71382a21b6
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-20 23:48:43 +0430

    feat(attrs/head_related): add HeadHref attribute struct

--- include/gvizard/attrs/head_related.hpp
+++ include/gvizard/attrs/head_related.hpp
@@ -1,8 +1,11 @@
 #ifndef GVIZARD_ATTRS_HEAD_RELATED_HPP_
 #define GVIZARD_ATTRS_HEAD_RELATED_HPP_
 
+#include <string>
+
 #include <gvizard/attribute.hpp>
 #include <gvizard/attrtypes/point.hpp>
+#include <gvizard/attrtypes/escstring.hpp>
 
 namespace gvizard::attrs {
 
@@ -51,6 +54,28 @@ struct HeadClip final : public AttributeBase<HeadClip, bool> {
   }
 
   constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+
+struct HeadHref final
+  : public AttributeBase<HeadHref, attrtypes::EscString<std::string>>
+{
+  using value_type = attrtypes::EscString<std::string>;
+
+  constexpr static const char * const name = "headhref";
+
+  explicit HeadHref() : AttributeBase(get_default_value()) {}
+  explicit HeadHref(const value_type& value) : AttributeBase(value) {}
+  explicit HeadHref(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return value_type(""); }
+
+  static bool is_default(const value_type& value)
+  {
+    return value.get_format_ref().empty();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
 };
 
 }  // namespace gvizard::attrs
