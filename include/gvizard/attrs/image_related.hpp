016c0bcf9bed1bdf94c4f1a3679358fafb312f2c
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-21 00:57:46 +0430

    feat(attrs/image_related): add Image attribute struct

---
+++ include/gvizard/attrs/image_related.hpp
@@ -0,0 +1,31 @@
+#ifndef GVIZARD_ATTRS_IMAGE_RELATED_HPP_
+#define GVIZARD_ATTRS_IMAGE_RELATED_HPP_
+
+#include <string>
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct Image final : public AttributeBase<Image, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "image";
+
+  explicit Image() : AttributeBase(get_default_value()) {}
+  explicit Image(const value_type& value) : AttributeBase(value) {}
+  explicit Image(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return ""; }
+
+  static bool is_default(const value_type& value) noexcept
+  {
+    return value.empty();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_IMAGE_RELATED_HPP_
