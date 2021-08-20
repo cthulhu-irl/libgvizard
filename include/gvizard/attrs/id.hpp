bfcc91410f1ea296017c9b7ad0e0349a9867446b
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-21 00:49:25 +0430

    feat(attrs/id): add Id attribute struct

---
+++ include/gvizard/attrs/id.hpp
@@ -0,0 +1,34 @@
+#ifndef GVIZARD_ATTRS_HREF_HPP_
+#define GVIZARD_ATTRS_HREF_HPP_
+
+#include <string>
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/escstring.hpp>
+
+namespace gvizard::attrs {
+
+struct Id final
+  : public AttributeBase<Id, attrtypes::EscString<std::string>>
+{
+  using value_type = attrtypes::EscString<std::string>;
+
+  constexpr static const char * const name = "id";
+
+  explicit Id() : AttributeBase(get_default_value()) {}
+  explicit Id(const value_type& value) : AttributeBase(value) {}
+  explicit Id(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return value_type(""); }
+
+  static bool is_default(const value_type& value)
+  {
+    return value.get_format_ref().empty();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_HREF_HPP_
