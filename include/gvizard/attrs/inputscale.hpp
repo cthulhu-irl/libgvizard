c8a68002891e6f178623a1490e9894c99262a77f
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-21 11:46:59 +0430

    feat(attrs/inputscale): add InputScale attribute struct

---
+++ include/gvizard/attrs/inputscale.hpp
@@ -0,0 +1,38 @@
+#ifndef GVIZARD_ATTRS_INPUTSCALE_HPP_
+#define GVIZARD_ATTRS_INPUTSCALE_HPP_
+
+#include <optional>
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct InputScale final
+  : public AttributeBase<InputScale, std::optional<double>>
+{
+  using value_type = std::optional<double>;
+
+  constexpr static const char * const name = "inputscale";
+
+  constexpr explicit InputScale() : AttributeBase(get_default_value()) {}
+  constexpr explicit InputScale(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return std::nullopt;
+  }
+
+  constexpr static bool is_default(const value_type& value) noexcept
+  {
+    return !value;
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_INPUTSCALE_HPP_
