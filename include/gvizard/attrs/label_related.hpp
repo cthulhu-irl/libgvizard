d4d1ba284c77bcafd06b135123bf9fddac3d4806
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-21 13:57:57 +0430

    feat(attrs/label_related): add LabelDistance attribute struct

--- include/gvizard/attrs/label_related.hpp
+++ include/gvizard/attrs/label_related.hpp
@@ -73,6 +73,31 @@ struct LabelAngle final : public AttributeBase<LabelAngle, double> {
   }
 };
 
+
+struct LabelDistance final : public AttributeBase<LabelDistance, double>
+{
+  using value_type = double;
+
+  constexpr static const char * const name = "labelangle";
+
+  constexpr explicit LabelDistance() : AttributeBase() {}
+  constexpr explicit LabelDistance(value_type value)
+    : AttributeBase(value)
+  {}
+
+  constexpr static value_type get_default_value() noexcept { return 1.; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= 0.;
+  }
+};
+
 }  // namespace gvizard::attrs
 
 #endif  // GVIZARD_ATTRS_LABEL_RELATED_HPP_
