fa02c877d698be91b33db37959611ece9b765c58
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-22 19:38:07 +0430

    feat(attrs/level_related.hpp): add LevelsGap attribute struct

--- include/gvizard/attrs/level_related.hpp
+++ include/gvizard/attrs/level_related.hpp
@@ -31,6 +31,25 @@ struct Levels final : public AttributeBase<Levels, unsigned int> {
   }
 };
 
+
+struct LevelsGap final : public AttributeBase<LevelsGap, double> {
+  using value_type = double;
+
+  constexpr static const char * const name = "levelsgap";
+
+  constexpr explicit LevelsGap() : AttributeBase() {}
+  constexpr explicit LevelsGap(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return 0.; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
 }  // namespace gvizard::attrs
 
 #endif  // GVIZARD_ATTRS_LEVEL_RELATED_HPP_
