6af1690063fc96f03fcbc1e121470bf91b70042e
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 03:54:06 +0430

    feat(attrs/logical_related.hpp): add LHeight attribute struct

--- include/gvizard/attrs/logical_related.hpp
+++ include/gvizard/attrs/logical_related.hpp
@@ -2,6 +2,7 @@
 #define GVIZARD_ATTRS_LOGICAL_RELATED_HPP_
 
 #include <string>
+#include <optional>
 
 #include <gvizard/attribute.hpp>
 
@@ -21,6 +22,30 @@ struct LHead final : public AttributeBase<LHead, std::string> {
   static bool is_default(const value_type& value) { return value.empty(); }
 
   static bool constraint(const value_type&) noexcept { return true; }
+};
+
+
+struct LHeight final
+  : public AttributeBase<LHeight, std::optional<double>>
+{
+  using value_type = std::optional<double>;
+
+  constexpr static const char * const name = "lheight";
+
+  constexpr explicit LHeight() : AttributeBase() {}
+  constexpr explicit LHeight(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return std::nullopt;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
 };
 
 }  // namespace gvizard::attrs
