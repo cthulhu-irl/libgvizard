6331eb5802b055ceab28b01cff21af75fd578734
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 04:00:53 +0430

    feat(attrs/logical_related.hpp): add LHead, LWidth and LPosition attribute structs

--- include/gvizard/attrs/logical_related.hpp
+++ include/gvizard/attrs/logical_related.hpp
@@ -5,6 +5,7 @@
 #include <optional>
 
 #include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/point.hpp>
 
 namespace gvizard::attrs {
 
@@ -25,6 +26,23 @@ struct LHead final : public AttributeBase<LHead, std::string> {
 };
 
 
+struct LTail final : public AttributeBase<LTail, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "ltail";
+
+  explicit LTail() : AttributeBase() {}
+  explicit LTail(const value_type& value) : AttributeBase(value) {}
+  explicit LTail(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return ""; }
+
+  static bool is_default(const value_type& value) { return value.empty(); }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+
 struct LHeight final
   : public AttributeBase<LHeight, std::optional<double>>
 {
@@ -34,6 +52,55 @@ struct LHeight final
 
   constexpr explicit LHeight() : AttributeBase() {}
   constexpr explicit LHeight(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return std::nullopt;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+
+struct LWidth final
+  : public AttributeBase<LWidth, std::optional<double>>
+{
+  using value_type = std::optional<double>;
+
+  constexpr static const char * const name = "lwidth";
+
+  constexpr explicit LWidth() : AttributeBase() {}
+  constexpr explicit LWidth(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return std::nullopt;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+
+using LPositionType = std::optional<attrtypes::Point2D<double>>;
+
+struct LPosition final : public AttributeBase<LPosition, LPositionType>
+{
+  using value_type = LPositionType;
+
+  constexpr static const char * const name = "lposition";
+
+  constexpr explicit LPosition() : AttributeBase() {}
+  constexpr explicit LPosition(value_type value) : AttributeBase(value) {}
 
   constexpr static value_type get_default_value() noexcept
   {
