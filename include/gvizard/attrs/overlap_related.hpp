9b15633d2f3a3013e6e1ef576912596f55486abb
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 13:19:04 +0430

    feat(attrs/overlap_related): add OverlapScaling attribute struct

--- include/gvizard/attrs/overlap_related.hpp
+++ include/gvizard/attrs/overlap_related.hpp
@@ -29,6 +29,31 @@ struct Overlap final : public AttributeBase<Overlap, OverlapType> {
   static bool constraint(const value_type&) noexcept { return true; }
 };
 
+
+struct OverlapScaling final : public AttributeBase<OverlapScaling, double>
+{
+  using value_type = double;
+
+  constexpr static const char * const name = "overlap_scaling";
+
+  constexpr explicit OverlapScaling() : AttributeBase() {}
+  constexpr explicit OverlapScaling(value_type value)
+    : AttributeBase(value)
+  {}
+
+  constexpr static value_type get_default_value() noexcept { return -4.; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= -10000000000;
+  }
+};
+
 }  // namespace gvizard::attrs
 
 #endif  // GVIZARD_ATTRS_OVERLAP_RELATED_HPP_
