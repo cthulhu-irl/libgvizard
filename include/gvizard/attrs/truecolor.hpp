e4e1c25826b809484e62783645a02e9157fccea5
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 12:17:48 +0430

    feat(attrs/truecolor): add TrueColor attribute struct

---
+++ include/gvizard/attrs/truecolor.hpp
@@ -0,0 +1,28 @@
+#ifndef GVIZARD_ATTRS_TRUECOLOR_HPP_
+#define GVIZARD_ATTRS_TRUECOLOR_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct TrueColor final : public AttributeBase<TrueColor, bool> {
+  using value_type = bool;
+
+  constexpr static const char * const name = "truecolor";
+
+  constexpr explicit TrueColor() : AttributeBase() {}
+  constexpr explicit TrueColor(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return false; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_TRUECOLOR_HPP_
