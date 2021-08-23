af0c7f37013a0954b3eb7f696d46797681ac67ae
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 12:48:15 +0430

    feat(attrs/xlabel): add XLabel attribute struct

---
+++ include/gvizard/attrs/xlabel.hpp
@@ -0,0 +1,31 @@
+#ifndef GVIZARD_ATTRS_XLABEL_HPP_
+#define GVIZARD_ATTRS_XLABEL_HPP_
+
+#include <string>
+
+#include "gvizard/attribute.hpp"
+
+namespace gvizard::attrs {
+
+struct XLabel final : public AttributeBase<XLabel, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "xlabel";
+
+  explicit XLabel() : AttributeBase() {}
+  explicit XLabel(const value_type& value) : AttributeBase(value) {}
+  explicit XLabel(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return ""; }
+
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_XLABEL_HPP_
