ae8772bfac5e9482f2f873af2174d28121553f32
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 11:59:17 +0430

    feat(attrs/target): add Target attribute struct

---
+++ include/gvizard/attrs/target.hpp
@@ -0,0 +1,39 @@
+#ifndef GVIZARD_ATTRS_TARGET_HPP_
+#define GVIZARD_ATTRS_TARGET_HPP_
+
+#include <string>
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/escstring.hpp>
+
+namespace gvizard::attrs {
+
+struct Target final
+  : public AttributeBase<Target, attrtypes::EscString<std::string>>
+{
+  using value_type = attrtypes::EscString<std::string>;
+
+  constexpr static const char * const name = "target";
+
+  explicit Target() : AttributeBase() {}
+  explicit Target(const value_type& value) : AttributeBase(value) {}
+  explicit Target(value_type&& value)
+    : AttributeBase(std::move(value))
+  {}
+
+  static value_type get_default_value() { return value_type(""); }
+
+  constexpr static bool is_default(const value_type& value) noexcept
+  {
+    return value.get_format_ref().empty();
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_TARGET_HPP_
