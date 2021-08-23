5465514c2413dff123aed77a7fbe6f388b08e559
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-23 08:17:26 +0430

    feat(attrs/tail_related.hpp): add TailHref attribute struct

--- include/gvizard/attrs/tail_related.hpp
+++ include/gvizard/attrs/tail_related.hpp
@@ -1,10 +1,12 @@
 #ifndef GVIZARD_ATTRS_TAIL_RELATED_HPP_
 #define GVIZARD_ATTRS_TAIL_RELATED_HPP_
 
+#include <string>
 #include <optional>
 
 #include <gvizard/attribute.hpp>
 #include <gvizard/attrtypes/point.hpp>
+#include <gvizard/attrtypes/label.hpp>
 
 namespace gvizard::attrs {
 
@@ -53,6 +55,33 @@ struct TailClip final : public AttributeBase<TailClip, bool> {
   }
 
   constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+
+struct TailHref final
+  : public AttributeBase<TailHref, attrtypes::Label<std::string>>
+{
+  using value_type = attrtypes::Label<std::string>;
+
+  constexpr static const char * const name = "tailhref";
+
+  explicit TailHref() : AttributeBase() {}
+  explicit TailHref(const value_type& value) : AttributeBase(value) {}
+  explicit TailHref(value_type&& value)
+    : AttributeBase(std::move(value))
+  {}
+
+  static value_type get_default_value() { return value_type(""); }
+
+  constexpr static bool is_default(const value_type& value) noexcept
+  {
+    return value.get_format_ref().empty();
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
 };
 
 }  // namespace gvizard::attrs
