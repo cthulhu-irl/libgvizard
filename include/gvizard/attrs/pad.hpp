82e0b55b408583b39cdfcf6ca9ecab28389fd818
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-24 16:52:36 +0430

    feat(attrs/pad): add Pad attribute struct

---
+++ include/gvizard/attrs/pad.hpp
@@ -0,0 +1,36 @@
+#ifndef GVIZARD_ATTRS_PAD_HPP_
+#define GVIZARD_ATTRS_PAD_HPP_
+
+#include <variant>
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/point.hpp>
+
+namespace gvizard::attrs {
+
+using PadType = std::variant<double, attrtypes::Point2D<double>>;
+
+struct Pad final : public AttributeBase<Pad, PadType> {
+  using value_type = PadType;
+
+  constexpr static const char * const name = "pad";
+
+  constexpr explicit Pad() : AttributeBase() {}
+  constexpr explicit Pad(const value_type& value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return .0555; }
+
+  constexpr static bool is_default(const value_type& value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  /// GVIZARD_ATTRS_PAD_HPP_
