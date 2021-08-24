bfb5d695108cad1d3e0ca0e51c8df91b43eecea0
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-24 15:31:22 +0430

    feat(attrs/packmode): add PackMode attribute struct

---
+++ include/gvizard/attrs/packmode.hpp
@@ -0,0 +1,36 @@
+#ifndef GVIZARD_ATTRS_PACKMODE_HPP_
+#define GVIZARD_ATTRS_PACKMODE_HPP_
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/packmode.hpp>
+
+namespace gvizard::attrs {
+
+struct PackMode final
+  : public AttributeBase<PackMode, attrtypes::PackMode>
+{
+  using value_type = attrtypes::PackMode;
+
+  constexpr static const char * const name = "packmode";
+
+  constexpr explicit PackMode() : AttributeBase() {}
+  constexpr explicit PackMode(const value_type& value)
+    : AttributeBase(value)
+  {}
+
+  constexpr static value_type get_default_value() noexcept { return {}; }
+
+  constexpr static bool is_default(const value_type& value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(const value_type&) noexcept
+  {
+    return true;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_PACKMODE_HPP_
