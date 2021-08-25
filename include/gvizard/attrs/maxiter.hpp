6be036329ca83e1cc355d002aecad47f0c7415e0
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 14:05:30 +0430

    feat(attrs/maxiter): add MaxIter attribute struct

---
+++ include/gvizard/attrs/maxiter.hpp
@@ -0,0 +1,35 @@
+#ifndef GVIZARD_ATTRS_MAXITER_HPP_
+#define GVIZARD_ATTRS_MAXITER_HPP_
+
+#include <optional>
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct MaxIter final
+  : public AttributeBase<MaxIter, std::optional<int>>
+{
+  using value_type = std::optional<int>;
+
+  constexpr static const char * const name = "maxiter";
+
+  constexpr explicit MaxIter() : AttributeBase() {}
+  constexpr explicit MaxIter(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return std::nullopt;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_MAXITER_HPP_
