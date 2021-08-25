808cf32fe8420268df80e77761b566baed394fbc
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 18:11:08 +0430

    feat(attrs/root): add Root attribute struct

---
+++ include/gvizard/attrs/root.hpp
@@ -0,0 +1,36 @@
+#ifndef GVIZARD_ATTRS_ROOT_HPP_
+#define GVIZARD_ATTRS_ROOT_HPP_
+
+#include <string>
+#include <variant>
+#include <optional>
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+using RootType = std::optional<std::variant<double, std::string>>;
+
+struct Root final : public AttributeBase<Root, RootType>
+{
+  using value_type = RootType;
+
+  constexpr static const char * const name = "root";
+
+  explicit Root() : AttributeBase() {}
+  explicit Root(const value_type& value) : AttributeBase(value) {}
+  explicit Root(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() noexcept { return std::nullopt; }
+
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_ROOT_HPP_
