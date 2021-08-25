dca04c87b2b0d38da405be653f3f1a7c2b86acb7
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 15:33:31 +0430

    feat(attrs/rankdir): add RankDir attribute struct

---
+++ include/gvizard/attrs/rankdir.hpp
@@ -0,0 +1,33 @@
+#ifndef GVIZARD_ATTRS_RANKDIR_HPP_
+#define GVIZARD_ATTRS_RANKDIR_HPP_
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/rank.hpp>
+
+namespace gvizard::attrs {
+
+struct RankDir final : public AttributeBase<RankDir, attrtypes::RankDir>
+{
+  using value_type = attrtypes::RankDir;
+
+  constexpr static const char * const name = "rankdir";
+
+  constexpr RankDir() : AttributeBase() {}
+  constexpr RankDir(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return attrtypes::RankDir::top_bottom;
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_RANKDIR_HPP_
