a16bcf1f1b2cd2ab6212072e08e2774b36be7ff0
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 14:29:21 +0430

    feat(attrs/newrank): add NewRank attribute struct

---
+++ include/gvizard/attrs/newrank.hpp
@@ -0,0 +1,28 @@
+#ifndef GVIZARD_ATTRS_NEWRANK_HPP_
+#define GVIZARD_ATTRS_NEWRANK_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct NewRank final : public AttributeBase<NewRank, bool> {
+  using value_type = bool;
+
+  constexpr static const char * const name = "newrank";
+
+  constexpr explicit NewRank() : AttributeBase() {}
+  constexpr explicit NewRank(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return false; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_NEWRANK_HPP_
