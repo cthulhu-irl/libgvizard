8105039692563abdb289372c050a68c8253673ba
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 15:44:59 +0430

    feat(attrs/ratio): add Ratio attribute struct

---
+++ include/gvizard/attrs/ratio.hpp
@@ -0,0 +1,39 @@
+#ifndef GVIZARD_ATTRS_RATIO_HPP_
+#define GVIZARD_ATTRS_RATIO_HPP_
+
+#include <string>
+#include <variant>
+#include <optional>
+
+#include <gvizard/attribute.hpp>
+
+#include <gvizard/utils.hpp>
+
+namespace gvizard::attrs {
+
+using RatioType = std::optional<std::variant<double, std::string>>;
+
+struct Ratio final
+  : public AttributeBase<Ratio, RatioType>
+{
+  using value_type = RatioType;
+
+  constexpr static const char * const name = "ratio";
+
+  explicit Ratio() : AttributeBase() {}
+  explicit Ratio(const value_type& value) : AttributeBase(value) {}
+  explicit Ratio(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() noexcept { return std::nullopt; }
+
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_RATIO_HPP_
