741dc65a3c73143a429feee8d4c49b68fecf104b
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 15:53:01 +0430

    feat(attrs/remincross): add ReminCross attribute struct

---
+++ include/gvizard/attrs/remincross.hpp
@@ -0,0 +1,28 @@
+#ifndef GVIZARD_ATTRS_REMINCROSS_HPP_
+#define GVIZARD_ATTRS_REMINCROSS_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct ReminCross final : public AttributeBase<ReminCross, bool> {
+  using value_type = bool;
+
+  constexpr static const char * const name = "remincross";
+
+  constexpr explicit ReminCross() : AttributeBase() {}
+  constexpr explicit ReminCross(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return true; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_REMINCROSS_HPP_
