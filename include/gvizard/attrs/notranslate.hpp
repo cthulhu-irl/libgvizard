bd3694b4f776defd91dec1a041b022c8e1d17a18
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 14:46:01 +0430

    feat(attrs/notranslate): add NoTranslate attribute struct

---
+++ include/gvizard/attrs/notranslate.hpp
@@ -0,0 +1,28 @@
+#ifndef GVIZARD_ATTRS_NOTRANSLATE_HPP_
+#define GVIZARD_ATTRS_NOTRANSLATE_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct NoTranslate final : public AttributeBase<NoTranslate, bool> {
+  using value_type = bool;
+
+  constexpr static const char * const name = "notranslate";
+
+  constexpr explicit NoTranslate() : AttributeBase() {}
+  constexpr explicit NoTranslate(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return false; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_NOTRANSLATE_HPP_
