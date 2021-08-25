7a40985888bb72944ac73b5667d53c9d66dfef68
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 18:36:53 +0430

    feat(attrs/sametail): add SameTail attribute struct

---
+++ include/gvizard/attrs/sametail.hpp
@@ -0,0 +1,31 @@
+#ifndef GVIZARD_ATTRS_SAMETAIL_HPP_
+#define GVIZARD_ATTRS_SAMETAIL_HPP_
+
+#include <string>
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct SameTail final : public AttributeBase<SameTail, std::string> {
+  using value_type = std::string;
+
+  constexpr static const char * const name = "sametail";
+
+  explicit SameTail() : AttributeBase() {}
+  explicit SameTail(const value_type& value) : AttributeBase(value) {}
+  explicit SameTail(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() { return ""; }
+
+  static bool is_default(const value_type& value) noexcept
+  {
+    return value.empty();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_SAMETAIL_HPP_
