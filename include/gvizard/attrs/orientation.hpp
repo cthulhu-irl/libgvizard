5aaf6435f430f4663c922dad8b6b50192b8b27df
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 15:11:23 +0430

    feat(attrs/orientation): add Orientation attribute struct

---
+++ include/gvizard/attrs/orientation.hpp
@@ -0,0 +1,47 @@
+#ifndef GVIZARD_ATTRS_ORIENTATION_HPP_
+#define GVIZARD_ATTRS_ORIENTATION_HPP_
+
+#include <string>
+#include <variant>
+
+#include <gvizard/attribute.hpp>
+
+#include <gvizard/utils.hpp>
+
+namespace gvizard::attrs {
+
+using OrientationType = std::variant<double, std::string>;
+
+struct Orientation final
+  : public AttributeBase<Orientation, OrientationType>
+{
+  using value_type = OrientationType;
+
+  constexpr static const char * const name = "orientation";
+
+  explicit Orientation() : AttributeBase() {}
+  explicit Orientation(const value_type& value) : AttributeBase(value) {}
+  explicit Orientation(value_type&& value)
+    : AttributeBase(std::move(value))
+  {}
+
+  static value_type get_default_value() noexcept { return 0.; }
+
+  static bool is_default(const value_type& value)
+  {
+    return value == get_default_value();
+  }
+
+  static bool constraint(const value_type& value) noexcept
+  {
+    return utils::LambdaVisit(
+      value,
+      [](double degree) { return 0. <= degree <= 360.; },
+      [](const std::string&) { return true; }
+    );
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_ORIENTATION_HPP_
