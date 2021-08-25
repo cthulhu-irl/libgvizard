0b4cc0bd59e00e7edd1fd5ea8bf80c469a790b49
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-25 18:13:40 +0430

    feat(attrs/rotate): add Rotate attribute struct

---
+++ include/gvizard/attrs/rotate.hpp
@@ -0,0 +1,28 @@
+#ifndef GVIZARD_ATTRS_ROTATE_HPP_
+#define GVIZARD_ATTRS_ROTATE_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct Rotate final : public AttributeBase<Rotate, int> {
+  using value_type = int;
+
+  constexpr static const char * const name = "rotate";
+
+  constexpr explicit Rotate() : AttributeBase() {}
+  constexpr explicit Rotate(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return 0; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_ROTATE_HPP_
