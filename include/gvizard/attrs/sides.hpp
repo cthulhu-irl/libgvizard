4f2999c9f76a82de9ca2b903350cb0115eadb19e
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-26 12:49:04 +0430

    feat(attrs/sides): add Sides attribute struct

---
+++ include/gvizard/attrs/sides.hpp
@@ -0,0 +1,31 @@
+#ifndef GVIZARD_ATTRS_SIDES_HPP_
+#define GVIZARD_ATTRS_SIDES_HPP_
+
+#include <gvizard/attribute.hpp>
+
+namespace gvizard::attrs {
+
+struct Sides final : public AttributeBase<Sides, int> {
+  using value_type = int;
+
+  constexpr static const char * const name = "sides";
+
+  constexpr explicit Sides() : AttributeBase() {}
+  constexpr explicit Sides(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept { return 4; }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type value) noexcept
+  {
+    return value >= 0;
+  }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_SIDES_HPP_
