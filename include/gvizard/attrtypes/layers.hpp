0026afa9a6d733ce98752b3a77d9aa3480ef7bcb
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-27 00:05:03 +0430

    feat(attrs/attrtypes): add LayerRange and LayerList type aliases

---
+++ include/gvizard/attrtypes/layers.hpp
@@ -0,0 +1,15 @@
+#ifndef GVIZARD_ATTRTYPES_LAYERS_HPP_
+#define GVIZARD_ATTRTYPES_LAYERS_HPP_
+
+#include <string>
+#include <vector>
+
+namespace gvizard::attrtypes {
+
+using LayerList = std::vector<std::string>;
+
+using LayerRange = std::vector<std::string>;
+
+}  // namespace gvizard::attrtypes
+
+#endif  // GVIZARD_ATTRTYPES_LAYERS_HPP_
