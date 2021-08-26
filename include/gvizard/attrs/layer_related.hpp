98649f5efce94a557b1f888e39383dbb518e7d98
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-27 00:09:14 +0430

    feat(attrs/layer_related): add Layer attribute struct

---
+++ include/gvizard/attrs/layer_related.hpp
@@ -0,0 +1,30 @@
+#ifndef GVIZARD_ATTRS_LAYER_RELATED_HPP_
+#define GVIZARD_ATTRS_LAYER_RELATED_HPP_
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/layers.hpp>
+
+namespace gvizard::attrs {
+
+struct Layer final : public AttributeBase<Layer, attrtypes::LayerRange> {
+  using value_type = attrtypes::LayerRange;
+
+  constexpr static const char * const name = "layer";
+
+  explicit Layer() noexcept : AttributeBase() {}
+  explicit Layer(const value_type& value) : AttributeBase(value) {}
+  explicit Layer(value_type&& value) : AttributeBase(std::move(value)) {}
+
+  static value_type get_default_value() noexcept { return {}; }
+
+  static bool is_default(const value_type& value) noexcept
+  {
+    return value.empty();
+  }
+
+  static bool constraint(const value_type&) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_LAYER_RELATED_HPP_
