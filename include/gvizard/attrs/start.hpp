aa5247cc3e161296e2ba87e4e94fbdd6434e3014
cthulhu-irl <xh4x0r3r@gmail.com>
2021-08-26 17:18:55 +0430

    feat(attrs/start): add Start attribute struct

---
+++ include/gvizard/attrs/start.hpp
@@ -0,0 +1,32 @@
+#ifndef GVIZARD_ATTRS_START_HPP_
+#define GVIZARD_ATTRS_START_HPP_
+
+#include <gvizard/attribute.hpp>
+#include <gvizard/attrtypes/starttype.hpp>
+
+namespace gvizard::attrs {
+
+struct Start final : public AttributeBase<Start, attrtypes::StartType> {
+  using value_type = attrtypes::StartType;
+
+  constexpr static const char * const name = "start";
+
+  constexpr Start() : AttributeBase() {}
+  constexpr Start(value_type value) : AttributeBase(value) {}
+
+  constexpr static value_type get_default_value() noexcept
+  {
+    return attrtypes::StartType{};
+  }
+
+  constexpr static bool is_default(value_type value) noexcept
+  {
+    return value == get_default_value();
+  }
+
+  constexpr static bool constraint(value_type) noexcept { return true; }
+};
+
+}  // namespace gvizard::attrs
+
+#endif  // GVIZARD_ATTRS_START_HPP_
