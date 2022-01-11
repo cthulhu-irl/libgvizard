814c2233dda34f10917dca21b0cf98c931111e4a
cthulhu-irl <xh4x0r3r@gmail.com>
2022-01-11 07:38:58 +0330

    refactor(attrtypes/addible): rewrite everything and let implicit conversion and add map method

--- include/gvizard/attrtypes/addible.hpp
+++ include/gvizard/attrtypes/addible.hpp
@@ -10,111 +10,28 @@ enum class AddibleSign : bool {
   addible = true
 };
 
-template <typename Derived, typename T>
-struct AddibleBase {
+template <typename T>
+struct Addible {
   using value_type = T;
 
   T value{};
   AddibleSign addible = AddibleSign::neutral;
 
-  constexpr static Derived
-  make(T value, AddibleSign sign = AddibleSign::neutral)
+  constexpr Addible(T value, AddibleSign sign = AddibleSign::neutral)
+    : value(std::move(value))
+    , addible(sign)
+  {}
+
+  constexpr T map(T other) const
   {
-    return Derived(std::move(value), sign);
+    return bool(addible) ? (value + std::move(other)) : other;
   }
 
   constexpr operator value_type() const { return value; }
-
-  constexpr bool operator==(const AddibleBase& other) const
-  {
-    return addible == other.addible && value == other.value;
-  }
-
-  constexpr bool operator!=(const AddibleBase& other) const
-  {
-    return addible != other.addible || value != other.value;
-  }
-
-  constexpr bool operator==(const value_type& other) const
-  {
-    return addible == AddibleSign::neutral && value == other;
-  }
-
-  constexpr bool operator!=(const value_type& other) const
-  {
-    return addible != AddibleSign::neutral || value != other;
-  }
-
-  constexpr Derived operator+() const&
-  {
-    return make( value, AddibleSign::addible );
-  }
-
-  constexpr Derived operator+() &&
-  {
-    return make( std::move(value), AddibleSign::addible );
-  }
-
-  constexpr Derived operator+(const Derived& rhs) const&
-  {
-    return make( value + rhs.value, addible );
-  }
-
-  constexpr Derived operator+(Derived&& rhs) const&
-  {
-    return make( value + std::move(rhs.value), addible );
-  }
-
-  constexpr Derived operator+(const Derived& rhs) &&
-  {
-    return make( std::move(value) + rhs.value, addible );
-  }
-
-  constexpr Derived operator+(Derived&& rhs) &&
-  {
-    return make( std::move(value) + std::move(rhs.value), addible );
-  }
 };
 
-// for arbitrary types from user
-template <typename T>
-struct Addible : public AddibleBase<Addible<T>, T> {
-  constexpr explicit
-  Addible(const T& value, AddibleSign sign = AddibleSign::neutral)
-    : AddibleBase<Addible<T>, T>{value, sign}
-  {}
-
-  constexpr explicit
-  Addible(T&& value, AddibleSign sign = AddibleSign::neutral)
-    : AddibleBase<Addible<T>, T>{std::move(value), sign}
-  {}
-};
-
-struct AddDouble : public AddibleBase<AddDouble, double> {
-  constexpr explicit
-  AddDouble(double value, AddibleSign sign = AddibleSign::neutral)
-    : AddibleBase{value, sign}
-  {}
-
-  constexpr AddDouble operator+(const AddDouble& rhs)
-  {
-    return make( value - rhs.value, addible );
-  }
-
-  constexpr AddDouble operator-(const AddDouble& rhs)
-  {
-    return make( value - rhs.value, addible );
-  }
-
-  constexpr AddDouble operator*(const AddDouble& rhs)
-  {
-    return make( value * rhs.value, addible );
-  }
-
-  constexpr AddDouble operator/(const AddDouble& rhs)
-  {
-    return make( value / rhs.value, addible );
-  }
+struct AddDouble : public Addible<double> {
+  using Addible::Addible;
 };
 
 }  // namespace gvizard::attrtypes
