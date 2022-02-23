baada0ee97029513ccf9b53aa32c244a6c50dca3
cthulhu-irl <xh4x0r3r@gmail.com>
2022-02-24 02:40:44 +0330

    test(attrtypes): init by some more EscString constructors and check empty string cases

--- tests/attrtypes.cpp
+++ tests/attrtypes.cpp
@@ -29,28 +29,41 @@ TEST_CASE("[attrtypes::AddDouble]")
 
 TEST_CASE("[attrtypes::EscString]")
 {
+  const EscString<> empty_escstring = "";
+
   const EscString<std::string> escstring =
-    "Graph: \\G, Node: \\N, Edge: \\E, Label: \\L, Head: \\H, Tail: \\T"_escstr;
+    "\\G as Graph: \\G, Node: \\N, Edge: \\E, "
+    "Label: \\L, Head: \\H, Tail: \\T"_escsv;
 
   const auto escnameset = EscNameSetRef{}.set_graph_str("A Graph")
-                                   .set_node_str("A Node")
-                                   .set_edge_str("An Edge")
-                                   .set_label_str("A Label")
-                                   .set_head_str("A Head")
-                                   .set_tail_str("A Tail");
+                                         .set_node_str("A Node")
+                                         .set_edge_str("An Edge")
+                                         .set_label_str("A Label")
+                                         .set_head_str("A Head")
+                                         .set_tail_str("A Tail");
 
   const char* expected_result =
-    "Graph: A Graph, Node: A Node, Edge: An Edge, "
+    "A Graph as Graph: A Graph, Node: A Node, Edge: An Edge, "
     "Label: A Label, Head: A Head, Tail: A Tail";
 
-  REQUIRE(escstring.get_occurences().graph == 1);
+  REQUIRE(escstring.get_occurences().graph == 2);
   REQUIRE(escstring.get_occurences().node  == 1);
   REQUIRE(escstring.get_occurences().edge  == 1);
   REQUIRE(escstring.get_occurences().label == 1);
   REQUIRE(escstring.get_occurences().head  == 1);
   REQUIRE(escstring.get_occurences().tail  == 1);
 
-  REQUIRE(escstring.get_occurences().sum() == 6);
+  REQUIRE(escstring.get_occurences().sum() == 7);
 
   REQUIRE(escstring.apply(escnameset) == expected_result);
+
+  REQUIRE(EscString<>().apply() == "");
+  REQUIRE(empty_escstring.apply() == "");
+  REQUIRE(empty_escstring.apply(escnameset) == "");
+  REQUIRE(empty_escstring.get_occurences().sum() == 0);
+}
+
+TEST_CASE("[attrtypes::PackMode]")
+{
+  //
 }
